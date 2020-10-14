398eb8cd8ddd0bb8721147cf01a5a368cfc20f46
lulubbelle <luli.icasatti@gmail.com>
2020-10-14 18:32:09 -0300

    Clase Movie

---
+++ Models/Movie.php
@@ -0,0 +1,62 @@
+<?php
+namespace Models;
+
+class Movie {
+    private $name;
+    private $score;
+    private $uploadingDate;
+    private $genres;
+    private $director;
+}
+
+public function __construct() {
+    $this->genres = array();
+}
+
+
+public function getName() {
+    return $this->name;
+}
+
+public function setName($name) {
+    $this->name = $name;
+}
+
+public function getScore() {
+    return $this->score;
+}
+
+public function setScore($score) {
+    $this->score = $score;
+}
+
+public function getUploadingDate() {
+    return $this->uploadingDate;
+}
+
+public function setUploadingDate($uploadingDate) {
+    $this->uploadingDate = $uploadingDate;
+}
+
+public function getGenres() {
+    return $this->genres;
+}
+
+public function getNameGenres() {
+    $string = "";
+    foreach($this->genres as $genre) {
+        $string = $string . " | " . $genre->getName();
+    }
+    $string = $string . " | "; 
+    return $string;
+}
+
+public function setGenres($genres) {
+    $this->genres = $genres;
+}
+
+public function addGenre($genre) {
+    array_push($this->genres, $genre);
+}
+}
+?>
